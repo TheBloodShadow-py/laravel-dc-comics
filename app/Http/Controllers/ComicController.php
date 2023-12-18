d5567a37782475296aaec7528bcd5f4740c1f007
Fabio <thebloodshadow14@gmail.com>
2023-12-18 14:37:10 +0100

    INIT - setup

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comic;
+use App\Http\Requests\StoreComicRequest;
+use App\Http\Requests\UpdateComicRequest;
+
+class ComicController extends Controller
+{
+    public function index()
+    {
+        return view("guest.index");
+    }
+}
